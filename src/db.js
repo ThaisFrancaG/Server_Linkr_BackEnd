dafe00dac76d26b8b1a373d30b1228e0836b85f7
Thais <tfrancag@gmail.com>
2022-03-22 09:58:38 -0300

    feat. Started repositorie and added basic archiquecture

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// const {Pool} = pg;
+// export const connection = new Pool({
+//     connectionString:process.env.DATABASE_URL;
+// })
+//URl é para conexão com o deploy
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { json } from "express";
+import router from "./routes/routeIndex.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}/`);
+});
