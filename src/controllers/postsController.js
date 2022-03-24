af3e1096b861c541bcb20f65fb60938aac4d898d
Thais <tfrancag@gmail.com>
2022-03-24 10:46:51 -0300

    feat. Added function to get and return posts list from database

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,23 +1,42 @@
+import { query } from "express";
 import { connection } from "../db.js";
 
-async function postPublication (req,res) {
-	const { link, description, id } = req.body;
+async function postPublication(req, res) {
+  const { link, description, id } = req.body;
 
-	if (!link) return res.sendStatus(400)
-	try {
-		const user = await connection.query(`SELECT * FROM users WHERE id = $1`, [id]);
-		if (!user.rowCount) return res.sendStatus(404)
+  if (!link) return res.sendStatus(400);
+  try {
+    const user = await connection.query(`SELECT * FROM users WHERE id = $1`, [
+      id,
+    ]);
+    if (!user.rowCount) return res.sendStatus(404);
 
-		await connection.query(`
+    await connection.query(
+      `
 			INSERT INTO posts (link, description, "userId")
 			VALUES ($1,$2,$3)
-		`, [link, description, id])
+		`,
+      [link, description, id]
+    );
 
-		return res.sendStatus(201)
-	}catch(error) {
-		return res.status(500).send(error)
-	}
-
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 }
 
-export { postPublication };
+async function getPublications(req, res) {
+  //é pra pegar todos os posts sem distinção
+  console.log("chegou aqui");
+  try {
+    let { rows: postList } = await connection.query(`
+		SELECT * FROM posts ORDER BY id DESC LIMIT 20
+		`);
+
+    res.status(200).send(postList);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
+export { postPublication, getPublications };
--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { postPublication } from "../controllers/postsController.js";
+import {
+  getPublications,
+  postPublication,
+} from "../controllers/postsController.js";
 import { validateSchema } from "../middleware/validateSchema.js";
 import { postSchema } from "../schemas/postSchema.js";
 
 const postsRouter = Router();
 
-postsRouter.post('/publish', validateSchema(postSchema), postPublication);
+postsRouter.post("/publish", validateSchema(postSchema), postPublication);
+postsRouter.get("/timeline", getPublications);
 
 export default postsRouter;
