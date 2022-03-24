2b6868f0ec8d4b63695f3f3191e6771ddaf20fb5
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-03-23 21:25:03 -0300

    Get User data route with token

--- src/routes/routeIndex.js
+++ src/routes/routeIndex.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 import logOutRouter from "./logOutRoute.js";
 import signInRouter from "./signInRouter.js";
 import signUpRouter from "./signUpRouter.js";
+import userRouter from "./userRoute.js";
 
 const router = Router();
 router.use(signInRouter);
 router.use(signUpRouter);
 router.use(logOutRouter);
+router.use(userRouter);
 export default router;
