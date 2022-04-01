d48c316150d30ebf994e6c34379c254c8745f9c0
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-04-01 18:31:17 -0300

    Fix username of resposts

--- src/controllers/postRepostController.js
+++ src/controllers/postRepostController.js
@@ -14,21 +14,22 @@ async function addRepost(req, res) {
     }
     const postInfo = checkPost[0];
 
-    const { rows : user} = await connection.query(`SELECT username FROM users WHERE id = $1`, [userId])
+    const { rows : user} = await connection.query(`SELECT * FROM users WHERE id = $1`, [userId])
 
     await connection.query(
       `
-			INSERT INTO posts (link, description, "userId", "isRepost","repostId")
-			VALUES ($1,$2,$3,$4,$5)
+			INSERT INTO posts (link, description, "userId", "isRepost","repostId", "repostUsername")
+			VALUES ($1,$2,$3,$4,$5,$6)
 		`,
-      [postInfo.link, postInfo.description, postInfo.userId, true, userId]
+      [postInfo.link, postInfo.description, postInfo.userId, true, userId, user[0].username]
     );
+
     await connection.query(
       `
-			INSERT INTO reposts ("postId","originalPosterId","repostId", "respostUsername")
+			INSERT INTO reposts ("postId","originalPosterId","repostId", "repostUsername")
 			VALUES ($1,$2,$3, $4)
 		`,
-      [postId, postInfo.userId, userId, user.username]
+      [postId, postInfo.userId, userId, user[0].username]
     );
     return res.sendStatus(201)
   } catch (error) {
