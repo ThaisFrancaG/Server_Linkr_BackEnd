416404dbdef7451a85aec2d0348c4b90e6ae9f34
rafaelarissa <rafaela.larissalj@gmail.com>
2022-04-01 16:49:55 -0300

    fix: fixed deletePosts validation

--- src/controllers/deletePostsController.js
+++ src/controllers/deletePostsController.js
@@ -2,10 +2,10 @@ import { connection } from "../db.js";
 
 async function deletePost(req, res) {
   const postId = req.params.id;
-  const auth = req.headers.authorization;
-  const token = auth?.replace("Bearer ", "");
+  // const auth = req.headers.authorization;
+  // const token = auth?.replace("Bearer ", "");
 
-  if (!token) return res.sendStatus(400);
+  // if (!token) return res.sendStatus(400);
 
   try {
     await connection.query(
